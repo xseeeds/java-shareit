dc6db0069a4edefc7784d1b172fd3926ea35d1ed
Sergey Kolganov <s86ko@yandex.ru>
2023-06-01 11:57:50 +0400

    ТЗ №13

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.header.HttpHeadersShareIt;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
@@ -13,16 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
 
-    public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
     private final ItemService itemService;
 
     @PostMapping
-    public ItemDto createItem(@RequestHeader(X_SHARER_USER_ID) long userId, @RequestBody ItemDto itemDto) {
+    public ItemDto createItem(@RequestHeader(HttpHeadersShareIt.X_SHARER_USER_ID) long userId, @RequestBody ItemDto itemDto) {
         return itemService.createItem(userId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@PathVariable long itemId, @RequestHeader(X_SHARER_USER_ID) long userId, @RequestBody ItemDto itemDto) {
+    public ItemDto updateItem(@PathVariable long itemId, @RequestHeader(HttpHeadersShareIt.X_SHARER_USER_ID) long userId, @RequestBody ItemDto itemDto) {
         return itemService.updateItem(userId, itemId, itemDto);
     }
 
@@ -32,18 +32,18 @@ public class ItemController {
     }
 
     @GetMapping
-    public List<ItemDto> getListItemsDtoByUser(@RequestHeader(X_SHARER_USER_ID) long userId) {
+    public List<ItemDto> getListItemsDtoByUser(@RequestHeader(HttpHeadersShareIt.X_SHARER_USER_ID) long userId) {
         return itemService.getListItemsDtoByUser(userId);
     }
 
     @GetMapping("/search") //?text={text}
     public List<ItemDto> getItemIsNotRentedByNameOrDescription(@RequestParam(value = "text") String text,
-                                                               @RequestHeader(X_SHARER_USER_ID) long userId) {
+                                                               @RequestHeader(HttpHeadersShareIt.X_SHARER_USER_ID) long userId) {
         return itemService.getItemIsNotRentedByNameOrDescription(text, userId);
     }
 
     @DeleteMapping("/{itemId}")
-    public void deleteItemById(@RequestHeader(X_SHARER_USER_ID) long userId,
+    public void deleteItemById(@RequestHeader(HttpHeadersShareIt.X_SHARER_USER_ID) long userId,
                            @PathVariable long itemId) {
         itemService.deleteItem(userId, itemId);
     }
